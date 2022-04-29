4c33a66f7b4d1490c132a606c1e0bd4cc32ee13d
John Moore <jmoore@zedstar.org>
2022-04-29 18:01:34 +0100

    use ocamlformat

--- lib/db.ml
+++ lib/db.ml
@@ -1,21 +1,19 @@
 open Lwt.Infix
-
-module Git_store_json_value =
-  (Irmin_unix.Git.FS.KV)(Irmin.Contents.Json_value)
+module Git_store_json_value = Irmin_unix.Git.FS.KV (Irmin.Contents.Json_value)
 module Store = Git_store_json_value
-module Proj = (Irmin.Json_tree)(Store)
+module Proj = Irmin.Json_tree (Store)
 
 let repository_author = ref "miiify.rocks"
-
 let info message = Irmin_unix.info ~author:!repository_author "%s" message
 
-type t = {db: Store.t Lwt.t }
+type t = { db : Store.t Lwt.t }
 
-let create ~fname ~author = 
+let create ~fname ~author =
   repository_author := author;
   let config = Irmin_git.config ~bare:true fname in
   let repo = Store.Repo.v config in
-  let branch = repo >>= Store.master in { db = branch }
+  let branch = repo >>= Store.master in
+  { db = branch }
 
 let add ~ctx ~key ~json ~message =
   ctx.db >>= fun branch -> Proj.set branch key json ~info:(info message)
@@ -25,23 +23,21 @@ let get ~ctx ~key = ctx.db >>= fun branch -> Proj.get branch key
 let delete ~ctx ~key ~message =
   ctx.db >>= fun branch -> Store.remove_exn branch key ~info:(info message)
 
-let exists ~ctx ~key =
-  ctx.db >>= fun branch -> Store.mem_tree branch key
+let exists ~ctx ~key = ctx.db >>= fun branch -> Store.mem_tree branch key
 
 let get_collection ~ctx ~key ~offset ~length =
-  ctx.db 
-  >>= fun branch -> Store.get_tree branch key 
-  >>= fun tree -> Store.Tree.list tree [] ~offset ~length
-  >>= Lwt_list.map_s (fun (k, _) -> get ~ctx ~key:(List.append key [k]))
+  ctx.db >>= fun branch ->
+  Store.get_tree branch key >>= fun tree ->
+  Store.Tree.list tree [] ~offset ~length
+  >>= Lwt_list.map_s (fun (k, _) -> get ~ctx ~key:(List.append key [ k ]))
   >|= Ezjsonm.list (fun x -> x)
 
 let count ~ctx ~key =
-  ctx.db >>= (fun branch -> (Store.list branch key) >|= List.length)
+  ctx.db >>= fun branch -> Store.list branch key >|= List.length
 
 let get_hash ~ctx ~key =
-  ctx.db 
-  >>= fun branch -> Store.hash branch key 
-  >|= fun hash ->
+  ctx.db >>= fun branch ->
+  Store.hash branch key >|= fun hash ->
   match hash with
-  | Some(hash) -> Some(Store.Git.Hash.to_hex hash)
+  | Some hash -> Some (Store.Git.Hash.to_hex hash)
   | None -> None
--- lib/db.mli
+++ lib/db.mli
@@ -2,16 +2,15 @@ type t
 
 val create : fname:string -> author:string -> t
 
-val add : ctx:t -> key:string list -> json:Ezjsonm.value -> message:string -> unit Lwt.t
+val add :
+  ctx:t -> key:string list -> json:Ezjsonm.value -> message:string -> unit Lwt.t
 
 val get : ctx:t -> key:string list -> Ezjsonm.value Lwt.t
-
 val delete : ctx:t -> key:string list -> message:string -> unit Lwt.t
-
 val exists : ctx:t -> key:string list -> bool Lwt.t
 
-val get_collection : ctx:t -> key:string list -> offset:int -> length:int -> Ezjsonm.value Lwt.t
+val get_collection :
+  ctx:t -> key:string list -> offset:int -> length:int -> Ezjsonm.value Lwt.t
 
 val count : ctx:t -> key:string list -> int Lwt.t
-
 val get_hash : ctx:t -> key:string list -> string option Lwt.t
