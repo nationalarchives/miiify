6c974a36e9f75a6a50d441e65257c504e04aa537
John Moore <jmoore@zedstar.org>
2023-02-21 18:22:13 +0000

    working on k8s deploy of miiifysearch

--- search/data.py
+++ search/data.py
@@ -15,6 +15,7 @@ class Data:
     def __init__(self, ctx):
         self.logger = ctx.logger
         self.repo = ctx.repo
+        self.index = ctx.index
         self.annotation_limit = ctx.annotation_limit
         self.remote_server = ctx.remote_server
         self.idx = None
@@ -24,9 +25,9 @@ class Data:
         return schema
 
     def __create_index__(self, schema):
-        if not os.path.exists("index"):
-            os.mkdir("index")
-        idx = create_in("index", schema)
+        if not os.path.exists(self.index):
+            os.mkdir(self.index)
+        idx = create_in(self.index, schema)
         self.idx = idx
         return idx
 
