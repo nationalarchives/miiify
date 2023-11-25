6f4d1f8ed96e2c11591d08aa9ee66faf06efffc3
John Moore <jmoore@zedstar.org>
2023-11-25 15:35:17 +0000

    get old integration code running

--- miiify/test/integration.rb
+++ miiify/test/integration.rb
@@ -2,8 +2,8 @@ require "airborne"
 
 Airborne.configure do |config|
   config.verify_ssl = false
-  config.base_url = "https://localhost"
-  #config.base_url = "https://localhost:8080"
+  #config.base_url = "https://localhost"
+  config.base_url = "http://localhost:8080"
 end
 
 describe "create container" do
