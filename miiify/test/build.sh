51aa3e89e3eaa0103e87413b140adb25c83e2105
John Moore <jmoore@zedstar.org>
2023-11-26 12:04:37 +0000

    being more strict on allowed paths and tweek to build script

--- miiify/test/build.sh
+++ miiify/test/build.sh
@@ -4,6 +4,7 @@ case $1 in
   "git"|"pack")
     cp ../lib/db.$1.ml ../lib/db.ml
     docker compose up $1 --build -d
-    rspec integration.rb -fd;;
+    rspec integration.rb -fd
+    docker compose down $1;;
   *) echo "supported backends: pack | git";;
 esac
