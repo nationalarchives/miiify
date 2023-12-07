43d8cfee46d50a00370b3fa3b53ff047154211b4
John Moore <jmoore@zedstar.org>
2023-12-07 11:49:18 +0000

    always rebuild

--- miiify/test/build.sh
+++ miiify/test/build.sh
@@ -3,7 +3,7 @@
 case $1 in
   "git"|"pack")
     cp ../lib/db.$1.ml ../lib/db.ml
-    docker compose up $1 --build -d
+    docker compose up $1 --build --force-recreate -d
     rspec integration.rb -fd
     docker compose down $1;;
   *) echo "supported backends: pack | git";;
