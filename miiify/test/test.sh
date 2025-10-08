017a9686c760f3a256d737fcdfdbd912ab263911
John Moore <jmoore@zedstar.org>
2025-10-08 11:59:41 +0100

    run unit tests also

--- miiify/test/test.sh
+++ miiify/test/test.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+# Run unit tests with dune
+run_unit_tests() {
+  echo "Running unit tests with dune..."
+  cd .. && dune runtest
+  if [ $? -ne 0 ]; then
+    echo "Unit tests failed. Stopping."
+    exit 1
+  fi
+  cd test
+  echo "Unit tests passed."
+}
+
 case $1 in
   "git")
+    run_unit_tests
     echo "Testing with Git backend..."
     MIIIFY_BACKEND=git docker compose up --force-recreate -d
     
@@ -28,6 +41,7 @@ case $1 in
     rspec integration.rb -fd
     docker compose down;;
   "pack")
+    run_unit_tests
     echo "Testing with Pack backend..."
     MIIIFY_BACKEND=pack docker compose up --force-recreate -d
     
@@ -54,6 +68,7 @@ case $1 in
     rspec integration.rb -fd
     docker compose down;;
   "both")
+    run_unit_tests
     echo "Testing Pack backend..."
     MIIIFY_BACKEND=pack docker compose up --force-recreate -d
     sleep 3
